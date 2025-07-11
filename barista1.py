533481e0223a2ba6b2dc52281668fac9071c5f41
Purvesh <Purvesh1635@Purvesh-MacBook.local>
2025-07-11 21:56:13 +0530

    barista1.py added to project

---
+++ barista1.py
@@ -0,0 +1,10 @@
+print ("Hello Welcome To Coffe House")
+name = input ("What is your name?\n")
+
+print("What would you like to order " + name)
+
+menu = input("1.TEA\n"
+"2.Coffe\n"
+"3.JOB\n")
+print ("your " + menu + " will be ready in few minutes " + name)
+
