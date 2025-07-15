ef3e3aab616736e8e864d97763ca286653dd6162
Purvesh <Purvesh1635@Purvesh-MacBook.local>
2025-07-15 18:24:18 +0530

    names.txt added

--- TEST.py
+++ TEST.py
@@ -1,6 +1,30 @@
 
+#adding hastag before something is just like a not for me or else who is reading code cause terminal wont read it
+#print("""i am iron man.  
+#no iam tony stark.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#no i am poppy""")
+#print("I am Iron Man " + "No I am Tony Stark " + "No I am Poppy")
+#print("I am Iron Man \n" + "No I am Tony Stark \n" + "No I am Poppy")
 #print("fuck what they say"*500)
-print("Hello, welcome to my project")
+print("Hello, welcome to my Cafe")
 name = input("what is your name?\n->>")
 print ("Hello " + name + ",thank you so much for checking out my project.")
 #1.give coustermer a menu
---
+++ maths1.py
@@ -0,0 +1,7 @@
+name = "Purvesh"
+age = 19
+actual_age = 19.6
+math = (5/6 + 6**5 - 8+9)
+results = (age + actual_age + math)
+print (results)
+
