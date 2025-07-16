25fff652ada69e9b4a3d3eefe64a0bfa3a9642e5
Purvesh <Purvesh1635@Purvesh-MacBook.local>
2025-07-16 12:17:09 +0530

    Simple_intrest.py added

---
+++ Simple intrest.py
@@ -0,0 +1,6 @@
+Principal = float (input("Enter Your Principal Amount\n"))
+Intrest = float(input("Enter Your Intrest Rate\n"))
+Time = float(input("Enter Your Time (in Years) Here\n"))
+Simple_interest = (Principal*Intrest*Time / 100)
+print (Simple_interest)
+
